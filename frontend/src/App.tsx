f54ee45ff4c04f9af9d9b16387cbc9440973d46f
shanyu <shanyu@deepinsighttech.com>
2025-04-22 16:03:58 +0800

    删除默认组件

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,44 +9,14 @@ import Info from './components/info';
 import Welcome from './components/welcome';
 
 const App: React.FC = () => {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
-
-    // 显示信息组件
-    const [showInfo, setShowInfo] = useState(false);
-
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+    
 
     return (
         <div id="App" style={{ display: 'flex', width: '100%', height: '100vh' }}>
             <Info />
             <Welcome />
         </div>
-        // <div id="App">
-        //     {showInfo ? (
-        //         <Info />
-        //     ) : (
-        //         <>
-        //             <img src={logo} id="logo" alt="logo"/>
-        //             <div id="result" className="result">{resultText}</div>
-        //             <div id="input" className="input-box">
-        //                 <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-        //                 <Button type="primary" onClick={greet}>Greet</Button>
-        //             </div>
-        //             <Button 
-        //                 type="primary" 
-        //                 style={{ marginTop: '20px' }} 
-        //                 onClick={() => setShowInfo(true)}
-        //             >
-        //                 显示信息
-        //             </Button>
-        //         </>
-        //     )}
-        // </div>
+        
     )
 }
 
